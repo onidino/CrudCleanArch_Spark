4622dc71b009873bb37ba6bedca61080b88f8a88
onidino <lucianodefilippe@gmail.com>
2022-03-27 18:51:10 -0300

    optimizing reformat

--- application/src/main/java/com/cleancrud/spark/config/Application.java
+++ application/src/main/java/com/cleancrud/spark/config/Application.java
@@ -9,12 +9,10 @@ public class Application implements SparkApplication {
 
   private final MainRouter mainRouter;
   private final Server server;
-  private final MySqlDatabase mySqlDatabase;
 
   public Application() {
     this.mainRouter = new MainRouter();
     this.server = new Server();
-    this.mySqlDatabase = new MySqlDatabase();
   }
 
   @Override
